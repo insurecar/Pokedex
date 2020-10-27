d59aa411329d882aa96493085e4ff2c6748a3aa5
insurecar <81600@ukr.net>
2020-10-27 17:25:23 +0200

    Updates

---
+++ precache-manifest.5599c8ec2c47e2c9292f8c438a7efa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4692a4eb2108930951cc10c80f47b0",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "1797d4f34a7ea31dc89a",
+    "url": "/Pokedex/static/css/main.93357b55.chunk.css"
+  },
+  {
+    "revision": "98181d1ed45a80183595",
+    "url": "/Pokedex/static/js/2.c64e7d5c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pokedex/static/js/2.c64e7d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1797d4f34a7ea31dc89a",
+    "url": "/Pokedex/static/js/main.88ed4fc4.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
